1b0475d3d423eabf2f1f9f65870e5b5d76bb8293
Haeun <hekoh99@naver.com>
2024-08-15 14:50:00 +0900

    update portfolio

---
+++ js/chart.js
@@ -0,0 +1,70 @@
+!function(){
+    var t = {
+        21:function(){
+            !function(){
+                const t={
+                    init(){
+                        const t={
+                            series:[40, 24, 15, 11, 10],
+                            labels:["C++","Python", "Java", "Javascript", "Golang"],
+                            chart:{type:"donut",sparkline:{enabled:!0}},
+                            colors:["#073763","#0b5394","#3d85c6", "#6fa8dc", "#9fc5e8"],
+                            tooltip:{y:{formatter(t){return t+"%"}}}};
+                            new ApexCharts(document.querySelector("#bsb-chart-language"),t).render()}
+                        };
+                            
+                function e(){
+                    t.init()
+                }
+
+                "loading"===document.readyState ? document.addEventListener("DOMContentLoaded",e) :e(),window.addEventListener("load",(function(){}),!1)}()}},e={};
+                function n(r){var o=e[r];if(void 0!==o)return o.exports;var a=e[r]={exports:{}};return t[r](a,a.exports,n),a.exports}n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,{a:e}),e},n.d=function(t,e){for(var r in e)n.o(e,r)&&!n.o(t,r)&&Object.defineProperty(t,r,{enumerable:!0,get:e[r]})},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},function(){"use strict";n(21)}()
+}();
+
+
+!function(){
+    var t = {
+        21:function(){
+            !function(){
+                const t={
+                    init(){
+                        const t={
+                            series:[40, 35, 12, 8, 5],
+                            labels:["STL", "Boost", "React", "Spring Boot", "Django"],
+                            chart:{type:"donut",sparkline:{enabled:!0}},
+                            colors:["#073763","#0b5394","#3d85c6", "#6fa8dc", "#9fc5e8"],
+                            tooltip:{y:{formatter(t){return t+"%"}}}};
+                            new ApexCharts(document.querySelector("#bsb-chart-framework"),t).render()}
+                        };
+                            
+                function e(){
+                    t.init()
+                }
+
+                "loading"===document.readyState ? document.addEventListener("DOMContentLoaded",e) :e(),window.addEventListener("load",(function(){}),!1)}()}},e={};
+                function n(r){var o=e[r];if(void 0!==o)return o.exports;var a=e[r]={exports:{}};return t[r](a,a.exports,n),a.exports}n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,{a:e}),e},n.d=function(t,e){for(var r in e)n.o(e,r)&&!n.o(t,r)&&Object.defineProperty(t,r,{enumerable:!0,get:e[r]})},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},function(){"use strict";n(21)}()
+}();
+
+
+!function(){
+    var t = {
+        21:function(){
+            !function(){
+                const t={
+                    init(){
+                        const t={
+                            series:[30, 25, 20, 20, 5],
+                            labels:["Git","Docker", "Wireshark", "GDB", "Strace"],
+                            chart:{type:"donut",sparkline:{enabled:!0}},
+                            colors:["#073763","#0b5394","#3d85c6", "#6fa8dc", "#9fc5e8"],
+                            tooltip:{y:{formatter(t){return t+"%"}}}};
+                            new ApexCharts(document.querySelector("#bsb-chart-tool"),t).render()}
+                        };
+                            
+                function e(){
+                    t.init()
+                }
+
+                "loading"===document.readyState ? document.addEventListener("DOMContentLoaded",e) :e(),window.addEventListener("load",(function(){}),!1)}()}},e={};
+                function n(r){var o=e[r];if(void 0!==o)return o.exports;var a=e[r]={exports:{}};return t[r](a,a.exports,n),a.exports}n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,{a:e}),e},n.d=function(t,e){for(var r in e)n.o(e,r)&&!n.o(t,r)&&Object.defineProperty(t,r,{enumerable:!0,get:e[r]})},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},function(){"use strict";n(21)}()
+}();
---
+++ res/bokeh/bokeh_web.js
@@ -0,0 +1,105 @@
+
+var w = window.innerWidth;
+var h = window.innerHeight;  
+
+class Point {
+  constructor(_x, _y){
+    this.x = _x;
+    this.y = _y;
+  }
+}
+
+class Ball {
+  
+  constructor(_center, _radius, _blurPoint){
+    this.center = _center;
+    this.radius = _radius;
+    this.flagX = true;
+    this.flagY = true;
+    this.tmp1 = random(-5, 5);
+    this.tmp2 = random(-5, 5);
+    this.r = random(0, 255);
+    this.g = random(0, 255);
+    this.b = random(0, 255);
+
+    this.blurPoint = _blurPoint;
+  }
+  
+  move(){
+    if (this.flagX){
+      this.center.x += this.tmp1;
+    }
+    else {
+      this.center.x -= this.tmp1;
+    }
+    if (this.flagY){
+      this.center.y += this.tmp2;
+    }
+    else {
+      this.center.y -= this.tmp2;
+    } 
+  
+    if (this.center.x + this.radius / 2 >= width){
+      this.flagX = false;
+      this.tmp1 = random(5);
+    }
+    else if (this.center.x - this.radius / 2 <= 0){
+      this.flagX = true;
+      this.tmp1 = random(5);
+    }
+  
+    if (this.center.y + this.radius / 2 >= height){
+      this.flagY = false;
+      this.tmp2 = random(5);
+    }
+    else if (this.center.y - this.radius / 2 <= 0){
+      this.flagY = true;
+      this.tmp2 = random(5);
+    }
+  }
+}
+
+var tmp1;
+var tmp2;
+var arrSize = 10;
+var ball = [];
+var cnt = 0;
+var fs;
+
+function setup(){
+  background(0);
+  createCanvas(w, h);
+  
+  for (var i=0;i<arrSize;i++){
+    center = new Point(random(0, width), random(0, height));
+    tmp_ball = new Ball(center, random(80, 300), random(5, 20));
+    ball.push(tmp_ball);
+  }  
+}
+
+function draw(){
+  clear();
+  background(0);
+  //fullscreen(true);
+  noStroke();
+  //noLoop();
+  blendMode(ADD);
+  
+  for(var i=0;i<arrSize;i++){
+    fill(ball[i].r, ball[i].g, ball[i].b);
+    ellipse(ball[i].center.x, ball[i].center.y, ball[i].radius, ball[i].radius);
+    drawingContext.filter = 'blur('+ball[i].blurPoint+'px)';
+    ball[i].move();
+  }
+}
+
+function mousePressed() {
+  center = new Point(mouseX, mouseY);
+  tmp_ball = new Ball(center, random(80, 300), random(5, 30));
+  ball.push(tmp_ball);
+  arrSize = ball.length;
+}
+
+//function mouseReleased() {
+//  noLoop();
+//}
